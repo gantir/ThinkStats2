8f856bea3ddf2db42760d0b78b24a27a3a99bf31
Ramjee Ganti <code@ramjeeganti.com>
2019-06-05 17:51:01 +0530

    added git extension

--- code/chap01ex.py
+++ code/chap01ex.py
@@ -13,7 +13,22 @@ import sys
 import nsfg
 import thinkstats2
 
+def ReadFemResp(dct_file='2002FemResp.dct',
+                   dat_file='2002FemResp.dat.gz'):
+    """Reads the NSFG respondent data.
 
+    dct_file: string file name
+    dat_file: string file name
+
+    returns: DataFrame
+    """
+    dct = thinkstats2.ReadStataDct(dct_file)
+    df = dct.ReadFixedWidth(dat_file, compression='gzip')
+    
+    return df
+
+def ValidatePregNum():
+    
 def main(script):
     """Tests the functions in this module.
 
